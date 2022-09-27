8e6b8b2cb3187ff49d90660602eb44ccdf5912ab
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-09-27 17:26:30 +0530

    Day 02

--- day02-springboot/src/main/java/com/intuit/Day02SpringbootApplication.java
+++ day02-springboot/src/main/java/com/intuit/Day02SpringbootApplication.java
@@ -1,9 +1,16 @@
 package com.intuit;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Day02SpringbootApplication implements CommandLineRunner {
@@ -23,9 +30,32 @@ public class Day02SpringbootApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(car1.getModel() + ", " + car1.getYear());
-		System.out.println(company);
-		System.out.println(person.getHobbies());
+//		System.out.println(car1.getModel() + ", " + car1.getYear());
+//		System.out.println(company);
+//		System.out.println(person.getHobbies());
+		
+		
+		String url = "http://localhost:8081/calc/add/12/12";
+		
+		//RestTemplate
+		//RestTemplate restTemplate = new RestTemplate();
+		//restTemplate.setMessageConverters(null);
+		//ResponseEntity<Integer> responseEntity = restTemplate.getForEntity(url, Integer.class);
+		//System.out.println("Result: " + responseEntity.getBody());
+		
+		
+		HttpClient httpClient = HttpClient.newBuilder()
+	            .version(HttpClient.Version.HTTP_1_1)
+	            .build();
+		
+		HttpRequest request = HttpRequest.newBuilder()
+				  .uri(new URI(url))
+				  .version(HttpClient.Version.HTTP_1_1)
+				  .GET()
+				  .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+		
 		
 	}
 }
