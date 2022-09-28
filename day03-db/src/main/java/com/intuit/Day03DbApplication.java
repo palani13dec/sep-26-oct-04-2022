62636944f3ae65982cf212a5981c9a6a29517df2
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-09-28 15:45:03 +0530

    Day 03

--- day03-db/src/main/java/com/intuit/Day03DbApplication.java
+++ day03-db/src/main/java/com/intuit/Day03DbApplication.java
@@ -1,5 +1,6 @@
 package com.intuit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +12,14 @@ public class Day03DbApplication implements CommandLineRunner {
 		SpringApplication.run(Day03DbApplication.class, args);
 	}
 
+	@Autowired
+	private PersonsDao personsDao;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		//personsDao.insertPerson("Ram", 32);
+		//System.out.println("Done");
+		System.out.println(personsDao.getNames());
 	}
 
 }
---
+++ day03-db/src/main/java/com/intuit/PersonsDao.java
@@ -0,0 +1,34 @@
+package com.intuit;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonsDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public void insertPerson(String name, int age) {
+		String sql = "insert into persons(name, age) values(?, ?)";
+		jdbcTemplate.update(sql, name, age);
+	}
+	
+	public List<String> getNames() {
+		String sql = "select name from persons";
+		return jdbcTemplate.queryForList(sql, String.class);
+	}
+	
+	public void updateAge(int id, int newAge) {
+		String sql = "update persons set age = ? where id = ?";
+		jdbcTemplate.update(sql, newAge, id);
+	}
+	
+	public void deletePerson(int id) {
+		String sql = "delete from persons where id = ?";
+		jdbcTemplate.update(sql, id);
+	}
+}
