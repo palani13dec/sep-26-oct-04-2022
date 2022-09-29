55c95267123bf3212a01e9a40884ef709c1124e7
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-09-29 15:54:16 +0530

    Day 04

---
+++ lab02-app/src/main/java/com/intuit/controller/AccountController.java
@@ -0,0 +1,50 @@
+package com.intuit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.intuit.entity.Account;
+import com.intuit.service.AccountService;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+
+	@PostMapping
+	public String createAccount(@RequestBody Account account) {
+		return accountService.createAccount(account);
+	}
+	
+	@GetMapping
+	public List<Account> getAllAccounts() {
+		return accountService.getAllAccounts();
+	}
+	
+	@GetMapping("/{accountNumber}")
+	public Account getAccount(@PathVariable int accountNumber) {
+		return accountService.getAccount(accountNumber);
+	}
+	
+	@DeleteMapping("/{accountNumber}")
+	public String deleteAccount(@PathVariable int accountNumber) {
+		return accountService.deleteAccount(accountNumber);
+	}
+	
+	@PatchMapping("/{accountNumber}/balance/{balance}") 
+	public String updateBalance(@PathVariable int accountNumber, @PathVariable int balance) {
+		return accountService.updateBalance(accountNumber, balance);
+	}
+	
+}
--- lab02-app/src/main/java/com/intuit/service/AccountService.java
+++ lab02-app/src/main/java/com/intuit/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.intuit.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +18,19 @@ public class AccountService {
 	private AccountRepository accountRepository;
 
 	public void deposit(int accountNumber, int amount) {
-		Account account = getAccount(accountNumber, amount);
+		Account account = getAccount(accountNumber);
 		account.setBalance(account.getBalance() + amount);
 		accountRepository.save(account);
 	}
 	
 	public void withdraw(int accountNumber, int amount) {
-		Account account = getAccount(accountNumber, amount);
+		Account account = getAccount(accountNumber);
 		//balance > amount is left out intentionally for the sake of simplicity
 		account.setBalance(account.getBalance() - amount);
 		accountRepository.save(account);
 	}
 	
-	private Account getAccount(int accountNumber, int amount) {
+	public Account getAccount(int accountNumber) {
 		Optional<Account> optAccount = accountRepository.findById(accountNumber);
 		if(optAccount.isPresent()) {
 			Account account = optAccount.get();
@@ -37,6 +38,29 @@ public class AccountService {
 		} else {
 			throw new AccountNotFoundException(accountNumber);
 		}
+	}
+
+	public List<Account> getAllAccounts() {
+		return accountRepository.findAll();
+	}
+
+
+	public String deleteAccount(int accountNumber) {
+		Account account = getAccount(accountNumber);
+		accountRepository.delete(account);
+		return "Deleted " + accountNumber;
+	}
+
+	public String updateBalance(int accountNumber, int balance) {
+		Account account = getAccount(accountNumber);
+		account.setBalance(balance);
+		accountRepository.save(account);
+		return "Updated balance";
+	}
+
+	public String createAccount(Account account) {
+		accountRepository.save(account);
+		return "Created " + account.getAccountNumber();
 	}
 
 }
