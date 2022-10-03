f4f5b90cd85dc90282c2ab58fe9d263b9a4fc200
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-10-03 15:00:20 +0530

    Day05

---
+++ day05/math-ui/src/main/java/com/intuit/MathUiApplication.java
@@ -0,0 +1,13 @@
+package com.intuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathUiApplication.class, args);
+	}
+
+}
